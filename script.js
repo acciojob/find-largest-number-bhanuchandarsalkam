c3a4374a031893bc69048a53cfdc2ae5d7d9f03b
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-10-13 15:18:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function findLargest(a, b, c) {
   //your code here
+	const temp1=a>b?(a>c?a:c):(b>c?b:c);
+	return temp1;
 }
 
-const num1 = parseInt(prompt("Enter First Number."));
-const num2 = parseInt(prompt("Enter Second Number."));
-const num3 = parseInt(prompt("Enter Third Number."));
+ const num1 = parseInt(prompt("Enter First Number."));
+ const num2 = parseInt(prompt("Enter Second Number."));
+ const num3 = parseInt(prompt("Enter Third Number."));
 alert(findLargest(num1, num2, num3));
+
